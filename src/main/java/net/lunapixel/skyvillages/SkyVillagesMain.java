5b5fa6637c1de86228a71c42db35a0a9efa1f246
Aureljz <catking060@gmail.com>
2023-10-07 21:48:02 -0300

    Initial commit

---
+++ src/main/java/net/lunapixel/skyvillages/SkyVillagesMain.java
@@ -0,0 +1,12 @@
+package net.lunapixel.skyvillages;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(SkyVillagesMain.MODID)
+public class SkyVillagesMain
+{
+    public static final String MODID = "skyvillages";
+
+    public SkyVillagesMain() {
+    }
+}
